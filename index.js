886ce4a0d9d6bfae63d2d1d1d7559c5aa1dfff04
Christian Smith <smith@anvil.io>
2015-03-28 09:14:32 -0700

    formatted log methods

--- index.js
+++ index.js
@@ -2,10 +2,26 @@
  * Dependencies
  */
 
-var nash      = require('nash')
+var pkg       = require('./package.json')
+  , nash      = require('nash')
   , commands  = require('./lib/commands')
   , cli       = nash()
   ;
+
+
+/**
+ * Settings
+ */
+
+cli.set({
+  pkg:
+    pkg,
+  website:
+    'http://anvil.io',
+  description:
+    'A modern authorization server built to\n' +
+    'authenticate your users and protect your APIs.',
+});
 
 
 /**
