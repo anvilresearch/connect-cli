f111c8d8cc40a9b9f186587720ccd53d8ea5c3f1
Christian Smith <smith@anvil.io>
2015-08-21 10:15:20 -0700

    feat(init): use latest package version automatically

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-conventional-changelog');
+  grunt.initConfig({
+
+    changelog: {
+      options: {
+        // Task-specific options go here.
+      }
+    }
+
+  });
+};
