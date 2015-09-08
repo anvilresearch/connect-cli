4afddbceb77a6375987ccba5feb7785c4c39d261
Vartan Simonian <vsimonianpress@gmail.com>
2015-09-07 20:40:37 -0700

    fix: Wait for registration callback before running commands
    Fixes a race condition where CLI commands would not be registered by the time the CLI attempts to execute them.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* global __dirname */
+
 /**
  * Dependencies
  */
@@ -23,33 +25,43 @@ cli.set({
 })
 
 /**
- * Initialize
+ * Register directories
+ *
  * Requires and registers a directory of plugins.
  */
 
-cli.initialize = function (directory) {
+cli.registerDirectories = function (directories, callback) {
   var plugins = []
-  var modules = fs.readdirSync(directory)
 
-  modules.forEach(function (mod) {
-    if (path.extname(mod) === '.js' && path.basename(mod) !== 'index.js') {
-      plugins.push({
-        register: require(path.join(directory, mod))
-      })
-    }
+  directories.forEach(function (directory) {
+    var modules = fs.readdirSync(directory)
+
+    modules.forEach(function (mod) {
+      if (path.extname(mod) === '.js' && path.basename(mod) !== 'index.js') {
+        plugins.push({
+          register: require(path.join(directory, mod))
+        })
+      }
+    })
   })
 
   cli.register(plugins, function (err) {
-    if (err) { }
+    callback(err)
   })
 }
 
 /**
- * Register Commands and Plugins
+ * Initialize
+ *
+ * Registers commands and plugins
  */
 
-cli.initialize(path.join(__dirname, 'lib', 'commands'))
-cli.initialize(path.join(__dirname, 'lib', 'plugins'))
+cli.initialize = function (callback) {
+  cli.registerDirectories([
+    path.join(__dirname, 'lib', 'commands'),
+    path.join(__dirname, 'lib', 'plugins')
+  ], callback)
+}
 
 /**
  * Exports
