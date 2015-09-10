8f8c5982fafee89efcb9ef082a1b92512133570e
Vartan Simonian <vsimonianpress@gmail.com>
2015-09-09 23:33:02 -0700

    feat: Add issuer management plugin

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ cli.initialize = function (callback) {
     path.join(__dirname, 'lib', 'plugins', 'env'),
     path.join(__dirname, 'lib', 'plugins', 'fs'),
     path.join(__dirname, 'lib', 'plugins', 'config'),
+    path.join(__dirname, 'lib', 'plugins', 'issuers'),
     path.join(__dirname, 'lib', 'commands')
   ], callback)
 }
